30a6ad3e5807627c8d6ff8d3ef3b75194b659b28
Ungweliant <40849503+Ungweliant@users.noreply.github.com>
2025-12-16 17:48:40 +0100

    Update GuildExport.lua

--- GuildExport.lua
+++ GuildExport.lua
@@ -1,10 +1,15 @@
 SLASH_GUILDEXPORT1="/gexport"
 SlashCmdList.GUILDEXPORT=function()
   GuildExportDB={}
+  local lines = {}
+  lines[#lines+1] = "Name,Rank,Level,Class,Note,OfficerNote"
+  
   for i=1,GetNumGuildMembers() do
     local n,r,x,l,c,y,nt,ont=GetGuildRosterInfo(i)
     GuildExportDB[i]={name=n,rank=r,level=l,class=c,note=nt,officernote=ont}
+    lines[#lines+1] = string.format("%s,%s,%d,%s,%s", n, r, l, c, nt, ont and "TRUE" or "FALSE")
   end
+  GuildExportCSV = table.concat(lines, "\n")
   print("Guild roster saved. Reload or logout to write file.")
 end
 
